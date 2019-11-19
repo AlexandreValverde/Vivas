bb003dd3118f583478aabfba10f8643d200cf2f1
Alex Mac <alex_valverde@hotmail.es>
2019-11-19 18:41:28 +0100

    Cliente Windows creado

---
+++ TareasVivas_Windows/TareasVivas/Form1.Designer.cs
@@ -0,0 +1,165 @@
+﻿namespace TareasVivas
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.lblNombre = new System.Windows.Forms.Label();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.lblEmail = new System.Windows.Forms.Label();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.txtPass = new System.Windows.Forms.TextBox();
+            this.lblPass = new System.Windows.Forms.Label();
+            this.chkAdmin = new System.Windows.Forms.CheckBox();
+            this.btnCrear = new System.Windows.Forms.Button();
+            this.lblInfo = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblNombre
+            // 
+            this.lblNombre.AutoSize = true;
+            this.lblNombre.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNombre.Location = new System.Drawing.Point(13, 13);
+            this.lblNombre.Name = "lblNombre";
+            this.lblNombre.Size = new System.Drawing.Size(60, 13);
+            this.lblNombre.TabIndex = 0;
+            this.lblNombre.Text = "NOMBRE";
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(16, 29);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(194, 20);
+            this.txtNombre.TabIndex = 1;
+            // 
+            // lblEmail
+            // 
+            this.lblEmail.AutoSize = true;
+            this.lblEmail.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblEmail.Location = new System.Drawing.Point(13, 65);
+            this.lblEmail.Name = "lblEmail";
+            this.lblEmail.Size = new System.Drawing.Size(44, 13);
+            this.lblEmail.TabIndex = 2;
+            this.lblEmail.Text = "EMAIL";
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Location = new System.Drawing.Point(16, 81);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(194, 20);
+            this.txtEmail.TabIndex = 3;
+            // 
+            // txtPass
+            // 
+            this.txtPass.Location = new System.Drawing.Point(16, 134);
+            this.txtPass.Name = "txtPass";
+            this.txtPass.PasswordChar = '*';
+            this.txtPass.Size = new System.Drawing.Size(194, 20);
+            this.txtPass.TabIndex = 5;
+            // 
+            // lblPass
+            // 
+            this.lblPass.AutoSize = true;
+            this.lblPass.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPass.Location = new System.Drawing.Point(13, 118);
+            this.lblPass.Name = "lblPass";
+            this.lblPass.Size = new System.Drawing.Size(91, 13);
+            this.lblPass.TabIndex = 4;
+            this.lblPass.Text = "CONTRASEÑA";
+            // 
+            // chkAdmin
+            // 
+            this.chkAdmin.AutoSize = true;
+            this.chkAdmin.Location = new System.Drawing.Point(16, 172);
+            this.chkAdmin.Name = "chkAdmin";
+            this.chkAdmin.Size = new System.Drawing.Size(117, 17);
+            this.chkAdmin.TabIndex = 6;
+            this.chkAdmin.Text = "ADMINISTRADOR";
+            this.chkAdmin.UseVisualStyleBackColor = true;
+            // 
+            // btnCrear
+            // 
+            this.btnCrear.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCrear.Location = new System.Drawing.Point(16, 204);
+            this.btnCrear.Name = "btnCrear";
+            this.btnCrear.Size = new System.Drawing.Size(194, 23);
+            this.btnCrear.TabIndex = 7;
+            this.btnCrear.Text = "CREAR";
+            this.btnCrear.UseVisualStyleBackColor = true;
+            this.btnCrear.Click += new System.EventHandler(this.BtnAdd_Click);
+            // 
+            // lblInfo
+            // 
+            this.lblInfo.AutoSize = true;
+            this.lblInfo.Location = new System.Drawing.Point(13, 241);
+            this.lblInfo.Name = "lblInfo";
+            this.lblInfo.Size = new System.Drawing.Size(0, 13);
+            this.lblInfo.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.btnCrear;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(227, 265);
+            this.Controls.Add(this.lblInfo);
+            this.Controls.Add(this.btnCrear);
+            this.Controls.Add(this.chkAdmin);
+            this.Controls.Add(this.txtPass);
+            this.Controls.Add(this.lblPass);
+            this.Controls.Add(this.txtEmail);
+            this.Controls.Add(this.lblEmail);
+            this.Controls.Add(this.txtNombre);
+            this.Controls.Add(this.lblNombre);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Añadir usuario";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblNombre;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.Label lblEmail;
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.TextBox txtPass;
+        private System.Windows.Forms.Label lblPass;
+        private System.Windows.Forms.CheckBox chkAdmin;
+        private System.Windows.Forms.Button btnCrear;
+        private System.Windows.Forms.Label lblInfo;
+    }
+}
+
---
+++ TareasVivas_Windows/TareasVivas/Form1.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TareasVivas
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        /// <summary>
+        /// Boton añadir pulsado.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void BtnAdd_Click(object sender, EventArgs e)
+        {
+            // Compruebo datos
+            if (CheckData())
+            {
+                // Conexion
+                HttpClient client = new HttpClient
+                {
+                    BaseAddress = new Uri("http://192.168.0.35:31416/tareasvivas/app/users/")
+                };
+
+                // Parametros
+                string p = string.Format("{0}?nombre={1}&email={2}&pass={3}", chkAdmin.Checked ? "nuevoadmin" : "nuevousuario", txtNombre.Text.Trim(), txtEmail.Text.Trim(), txtPass.Text.Trim());
+
+                // Añado Accept
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("text/plain"));
+
+                // Depuracion
+                Console.WriteLine("URL -> " + p);
+
+                // Obtengo datos
+                HttpResponseMessage response = client.GetAsync(p).Result;
+                if (response.IsSuccessStatusCode)
+                {
+                    // Actualizo info
+                    lblInfo.Text = "Usuario añadido!";
+                }
+                else
+                {
+                    // Depuracion
+                    Console.WriteLine("Codigo respuesta servirdor -> " + response.StatusCode);
+
+                    // Actualizo info
+                    lblInfo.Text = "Error al añadir usuario...";
+                }
+            }
+            else
+            {
+                // Actualizo info
+                lblInfo.Text = "Introduce todos los datos";
+            }
+        }
+
+        /// <summary>
+        /// Comprueba los datos.
+        /// </summary>
+        /// <returns></returns>
+        private bool CheckData()
+        {
+            if (string.IsNullOrWhiteSpace(txtNombre.Text)) return false;
+            if (string.IsNullOrWhiteSpace(txtEmail.Text)) return false;
+            if (string.IsNullOrWhiteSpace(txtPass.Text)) return false;
+            return true;
+        }
+    }
+}
